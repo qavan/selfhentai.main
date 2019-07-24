3072fe4e17758dbe038646d6e8c3842e61042547
qavan <pro100qavan@gmail.com>
2019-07-24 20:15:38 +0300

    css full reset added

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ error_reporting(-1);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/images/icon2407.png" type="image/png">
+    <link rel="stylesheet" href="/styles/reset.css" type="text/css">
     <link rel="stylesheet" href="/styles/login.css" type="text/css">
     <title>Pseudo Engine</title>
 </head>
