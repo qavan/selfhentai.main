fa8d0679070e80ec48784b2030b4aec25702535c
qavan <pro100qavan@gmail.com>
2019-07-31 00:10:25 +0300

    dark design starts;js connected;

---
+++ js/register.js
@@ -0,0 +1,13 @@
+$(window).resize(function() {
+    if ( $(window).width() >= 480 && $(window).height()>=480 ) {
+        var a = 1 + 2/9*$(window).height()/1980+2/9*$(window).width()/1080;
+        $(".gigaForm").css("transform","scale("+a.toFixed(1)+")");
+    }
+    else if ( $(window).width() <= 320 && $(window).height()<=480 ) {
+        var a = 1 - 2/9*$(window).height()/1980-2/9*$(window).width()/1080;
+        $(".gigaForm").css("transform","scale("+a.toFixed(1)+")");
+    }
+    else {
+        $(".gigaForm").css("transform","scale(1)");
+    }
+});
